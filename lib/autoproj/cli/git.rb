eb4376359d7128267facedd4f9553c06660f7ab9
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-05-02 16:01:59 -0300

    add some statistics subcommands

--- lib/autoproj/cli/git.rb
+++ lib/autoproj/cli/git.rb
@@ -6,36 +6,64 @@ module Autoproj
     module CLI
         class Git < InspectionTool
             def cleanup(user_selection, options = Hash.new)
-                initialize_and_load
-                source_packages, * =
-                    finalize_setup(user_selection,
-                                   non_imported_packages: :ignore)
-                git_packages = source_packages.map do |pkg_name|
-                    pkg = ws.manifest.find_autobuild_package(pkg_name)
-                    pkg if pkg.importer.kind_of?(Autobuild::Git)
-                end.compact
+                git_packages = resolve_selected_git_packages(user_selection)
+                run_parallel(git_packages) do |pkg, i|
+                    cleanup_package(pkg, " [#{i}/#{git_packages.size}]",
+                                    local: options[:local],
+                                    remove_obsolete_remotes: options[:remove_obsolete_remotes])
+                end
+            end
 
-                package_failures = []
-                pool = Concurrent::FixedThreadPool.new(4)
-                futures = git_packages.each_with_index.map do |pkg, i|
-                    Concurrent::Future.execute(executor: pool) do
-                        begin
-                            cleanup_package(pkg, " [#{i}/#{git_packages.size}]",
-                                            local: options[:local],
-                                            remove_obsolete_remotes: options[:remove_obsolete_remotes])
-                            nil
-                        rescue Autobuild::SubcommandFailed => e
-                            Autoproj.error "failed: #{e.message}"
-                            e
-                        end
+            MATCH_ALL  = proc { true }
+            MATCH_NONE = proc { }
+
+            def authors(options = Hash.new)
+                packages = resolve_selected_git_packages([])
+                all_lines = run_parallel(packages) do |pkg, _|
+                    pkg.importer.run_git(pkg, 'shortlog', '-s', '-e')
+                end.flatten
+
+                all_authors = Set.new
+                all_lines.each do |line|
+                    match = /^\s*\d+\s+(.+)$/.match(line)
+                    all_authors << match[1] if match
+                end
+                puts all_authors.to_a.sort.join("\n")
+            end
+
+            def extension_stats(options = Hash.new)
+                git_packages = resolve_selected_git_packages([])
+
+                stats = compute_stats(git_packages) do |pkg, filename|
+                    File.extname(filename)
+                end
+                display_stats(stats)
+            end
+
+            def author_stats(authors, options = Hash.new)
+                git_packages = resolve_selected_git_packages([])
+
+                include_filter =
+                    if options[:include]
+                        Regexp.new(options[:include], Regexp::IGNORECASE)
+                    else
+                        MATCH_ALL
+                    end
+
+                exclude_filter =
+                    if options[:exclude]
+                        Regexp.new(options[:exclude], Regexp::IGNORECASE)
+                    else
+                        MATCH_NONE
+                    end
+
+
+                stats = compute_stats(git_packages, "--use-mailmap", *authors.map { |a| "--author=#{a}" }) do |pkg, filename|
+                    if (include_filter === filename) && !(exclude_filter === filename)
+                        pkg.name
                     end
                 end
-                package_failures = futures.each(&:execute).map(&:value!).compact
-            rescue Interrupt => interrupt
-            ensure
-                pool.shutdown if pool
-                Autobuild::Reporting.report_finish_on_error(
-                    package_failures, on_package_failures: :raise, interrupted_by: interrupt)
+                display_stats(stats)
             end
 
             def git_clean_invalid_refs(pkg, progress)
@@ -98,6 +126,72 @@ module Autoproj
 
                 git_gc(pkg, progress)
                 git_repack(pkg, progress)
+            end
+
+            def resolve_selected_git_packages(user_selection)
+                initialize_and_load
+                source_packages, * =
+                    finalize_setup(user_selection,
+                                   non_imported_packages: :ignore)
+                source_packages.map do |pkg_name|
+                    pkg = ws.manifest.find_autobuild_package(pkg_name)
+                    pkg if pkg.importer.kind_of?(Autobuild::Git)
+                end.compact
+            end
+
+            def run_parallel(objects, &block)
+                pool = Concurrent::FixedThreadPool.new(4)
+                futures = objects.each_with_index.map do |obj, i|
+                    Concurrent::Future.execute(executor: pool) do
+                        begin
+                            result = yield(obj, i)
+                            [result, nil]
+                        rescue Autobuild::SubcommandFailed => e
+                            Autoproj.error "failed: #{e.message}"
+                            [nil, e]
+                        end
+                    end
+                end
+                result   = futures.each(&:execute).map(&:value!).compact
+                failures = result.map(&:last).compact
+                result.map(&:first)
+            rescue Interrupt => interrupt
+            ensure
+                pool.shutdown if pool
+                Autobuild::Reporting.report_finish_on_error(
+                    failures || [], on_package_failures: :raise, interrupted_by: interrupt)
+            end
+
+            def compute_stats(packages, *log_options)
+                all_runs = run_parallel(packages) do |pkg, _|
+                    lines = pkg.importer.run_git(
+                        pkg, 'log', *log_options, '--pretty=tformat:', '--numstat')
+                    [pkg, lines]
+                end
+
+                all_runs.each_with_object(Hash.new) do |(pkg, lines), stats|
+                    lines.each do |l|
+                        match = /^\s*(\d+)\s+(\d+)\s+(.*)/.match(l)
+                        if match && (key = yield(pkg, match[3]))
+                            key_stats = (stats[key] ||= [0, 0])
+                            key_stats[0] += Integer(match[1])
+                            key_stats[1] += Integer(match[2])
+                        end
+                    end
+                end
+            end
+
+            def display_stats(stats, io: STDOUT)
+                total_p, total_m = 0, 0
+                stats.keys.sort.each do |text|
+                    p, m = stats[text]
+                    total_p += p
+                    total_m += m
+                    unless p == 0 && m == 0
+                        io.puts format("+%6i -%6i %s", p, m, text)
+                    end
+                end
+                io.puts format("+%6i -%6i %s", total_p, total_m, "Total")
             end
         end
     end
--- lib/autoproj/cli/main_git.rb
+++ lib/autoproj/cli/main_git.rb
@@ -15,6 +15,40 @@ module Autoproj
                     cli.cleanup(*args)
                 end
             end
+
+            desc 'authors', 'list all authors (useful to tune a mailmap file)'
+            def authors
+                require 'autoproj/cli/git'
+                Autoproj.report(silent: true) do
+                    cli = Git.new
+                    _, options = cli.validate_options([], self.options)
+                    cli.authors(options)
+                end
+            end
+
+            desc 'ext-stats', 'show diffstat statistics on a per-extension basis'
+            def ext_stats
+                require 'autoproj/cli/git'
+                Autoproj.report(silent: true) do
+                    cli = Git.new
+                    _, options = cli.validate_options([], self.options)
+                    cli.extension_stats(options)
+                end
+            end
+
+            desc 'author-stats AUTHOR [PACKAGES]', 'show diffstat statistics about a given author'
+            option :include, type: :string,
+                desc: 'regular expression of file names that should be counted'
+            option :exclude, type: :string,
+                desc: 'regular expression of file names that should not be counted'
+            def author_stats(*authors)
+                require 'autoproj/cli/git'
+                Autoproj.report(silent: true) do
+                    cli = Git.new
+                    authors, options = cli.validate_options(authors, self.options)
+                    cli.author_stats(authors, options)
+                end
+            end
         end
     end
 end
